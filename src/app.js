ba7ebb9f3455baf214e6759620409ed2dd369cf8
Nathan <natedog7120@gmail.com>
2025-11-14 17:59:24 -0500

    grid tycoon

--- src/app.js
+++ src/app.js
@@ -46,10 +46,8 @@ async function startServer() {
     console.error("Failed to retrieve secret:", error);
   }
 
-  const PORT = process.env.PORT || 4000;
-  app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
-  });
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, '0.0.0.0');
 }
 
 startServer();
